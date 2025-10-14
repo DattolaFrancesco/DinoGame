e4bb40cd96b36ca45ece711fdf84ff90e9baaf0c
Francesco Dattola <dattolafrancescoo@gmail.com>
2025-10-14 23:39:46 +0200

    -function for using the 2 cactus;-added some function as score and gameover and refresh the page to play again

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
   let increase = 5;
   let isjumping = false;
   let goingup = false;
-  let maxalt = 150;
+  let maxalt = 115;
 
   // funzione salto
 
@@ -45,49 +45,96 @@
         }
     })
 
-    // selettore cactus1
+    // selettore cactus
 
     cactus1 = document.querySelector(".cactus1")
+    cactus2 = document.querySelector(".cactus2")
+    cactus3 = document.querySelector(".cactus3")
     contenitoreGioco = document.querySelector("#contenitore-gioco")
+
+    // selettore pulsante avvio game e titolo GameOver e rigioca e punteggio
+
+    buttonGame = document.querySelector(".buttonGame")
+    GameOver = document.querySelector(".h1G")
+    Riprova = document.querySelector(".riprova")
+    Punteggio = document.querySelector(".punteggio")
+    let score = 0
 
 
     // variabili spostamento verso sinistra
 
     let left = contenitoreGioco.offsetWidth;
-    
     cactus1.style.left = left + "px";
-    
-
+    cactus2.style.left = left + "px";
+    cactus3.style.left = left + "px";
     let goingleft = false;
-    
-    
+    let stopGame = false
+    let cactusArray = [cactus1,cactus2,cactus3];
+    let velocita = 0
+    //funzione per fare il cactus casuale
+
+    function randomcactus(){
+        let randomindex = Math.floor(Math.random()*cactusArray.length);
+        return cactusArray[randomindex];
+    }
+   
+
     //funzione movimento verso sinistra
 
-    function sinistra(){
-       if(goingleft){
-          
-        left -= 3;
-          cactus1.style.left = left + "px";
+    function sinistra(cactus,left){
+    if(stopGame){
+        left = contenitoreGioco.offsetWidth;
+        return;
     }
-    if(left + cactus1.offsetWidth > 0){
-        requestAnimationFrame(sinistra);
+    if(goingleft && velocita < 3 ){
+          
+        left -= 5;
+          cactus.style.left = left + "px";
+          cactus.style.display = "block" ;
+
+    }if(goingleft && velocita >= 3 && velocita <= 7){
+        
+        left -= 6;
+        cactus.style.left = left + "px";
+        cactus.style.display = "block" ;
+    }
+    if(goingleft && velocita >= 7 && velocita <= 15){
+         
+        left -= 7.5;
+        cactus.style.left = left + "px";
+        cactus.style.display = "block" ;
+    }
+    if(goingleft && velocita >= 15){
+         
+        left -= 9;
+        cactus.style.left = left + "px";
+        cactus.style.display = "block" ;
+    }
+    if(left + cactus.offsetWidth > 0){
+        requestAnimationFrame(() => sinistra(cactus,left));
         
         
     }
     else{
+        cactus.style.display = "none"
+        cactus = randomcactus()
         left = contenitoreGioco.offsetWidth;
-         requestAnimationFrame(sinistra);
-         
-         
-       }
+        score++;
+        velocita++;
+        aggiornapunteggio();
+        requestAnimationFrame(() => sinistra(cactus,left));
+    }
 
     }
 
-    document.addEventListener("mousedown", function(event){
-        if(event.button === 0 && !goingleft){
+    buttonGame.addEventListener("click", function(){
+        if(!goingleft){
             goingleft = true;
-            requestAnimationFrame(sinistra);
+            let cactus = randomcactus()
+            let left = contenitoreGioco.offsetWidth;
+            sinistra(cactus,left)
             requestAnimationFrame(loopGioco);
+            
             
         }
     })
@@ -96,21 +143,55 @@
     // funzione monitoraggio collisioni
 
     function loopGioco(){
+
          let dinoBox = dino.getBoundingClientRect();
          let cactus1Box = cactus1.getBoundingClientRect();
+         let cactus2Box = cactus2.getBoundingClientRect();
+         let cactus3Box = cactus3.getBoundingClientRect();
         if( 
             dinoBox.right > cactus1Box.left &&
             dinoBox.left < cactus1Box.right &&
             dinoBox.bottom > cactus1Box.top &&
-            dinoBox.top < cactus1Box.bottom
+            dinoBox.top < cactus1Box.bottom 
         ){
+            stopGame = true;
             left = contenitoreGioco.offsetWidth;
-            alert("Game Over!");
+            GameOver.style.display = "block"
             requestAnimationFrame(loopGioco);
-        }else{
+        }
+        if( dinoBox.right > cactus2Box.left &&
+            dinoBox.left < cactus2Box.right &&
+            dinoBox.bottom > cactus2Box.top &&
+            dinoBox.top < cactus2Box.bottom
+        ){
+            stopGame = true;
+            left = contenitoreGioco.offsetWidth;
+            GameOver.style.display = "block"
+            requestAnimationFrame(loopGioco);
+        }
+        if( dinoBox.right > cactus3Box.left &&
+            dinoBox.left < cactus3Box.right &&
+            dinoBox.bottom > cactus3Box.top &&
+            dinoBox.top < cactus3Box.bottom 
+        ){
+            stopGame = true;
+            left = contenitoreGioco.offsetWidth;
+            GameOver.style.display = "block"
+            requestAnimationFrame(loopGioco);
+        }
+        else{
             requestAnimationFrame(loopGioco);
         }
     }
 
+// funzione per rigiocare
 
+Riprova.addEventListener("click",function(){
+    location.reload();
+})
 
+// funzione punteggio
+
+function aggiornapunteggio(){
+    Punteggio.innerText = "Punteggio : " + score
+}
